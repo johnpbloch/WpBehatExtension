361054e05d15ee450dfe3aaa7b1792cdcdc70e56
John P Bloch <johnpbloch@gmail.com>
2015-11-24 16:05:01 -0600

    Move authentication rules into its own context

--- src/Context/WpContext.php
+++ src/Context/WpContext.php
@@ -8,30 +8,6 @@ use Behat\MinkExtension\Context\RawMinkContext;
 class WpContext extends RawMinkContext {
 
 	/**
-	 * @Given I am not logged in
-	 */
-	public function iAmNotLoggedIn() {
-		$this->getSession()->reset();
-		$this->getSession()->visit( wp_logout_url() );
-	}
-
-	/**
-	 * @Given I am logged in as :username with :password
-	 */
-	public function iAmLoggedInAs( $username, $password ) {
-		$this->getSession()->reset();
-		$login_url = wp_login_url();
-		$this->getSession()->visit( $login_url );
-		$currentPage = $this->getSession()->getPage();
-
-		$currentPage->fillField( 'user_login', $username );
-		$currentPage->fillField( 'user_pass', $password );
-		$currentPage->findButton( 'wp-submit' )->click();
-
-		\PHPUnit_Framework_Assert::assertNotEquals( $this->getSession()->getCurrentUrl(), $login_url );
-	}
-
-	/**
 	 * @Given /^Users exist:$/
 	 */
 	public function usersExist( TableNode $table ) {
